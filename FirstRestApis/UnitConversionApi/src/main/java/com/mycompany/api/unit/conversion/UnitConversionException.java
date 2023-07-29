f8a3df8fd77bcb13f129a07fb430526122558a37
Larry Osakwe <larryosak@gmail.com>
2023-07-28 23:02:34 -0700

    add km to mile converter

---
+++ FirstRestApis/UnitConversionApi/src/main/java/com/mycompany/api/unit/conversion/UnitConversionException.java
@@ -0,0 +1,14 @@
+package com.mycompany.api.unit.conversion;
+
+public class UnitConversionException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UnitConversionException(String message) {
+		super(message);
+	}
+
+}
