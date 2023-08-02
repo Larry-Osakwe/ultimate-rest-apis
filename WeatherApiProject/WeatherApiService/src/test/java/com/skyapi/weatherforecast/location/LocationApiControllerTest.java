9e5a99aaf7cb904f08f75828afc87c670ccf6ceb
Larry Osakwe <larryosak@gmail.com>
2023-08-02 16:31:27 -0700

    add unit test for adding location successfully

--- WeatherApiProject/WeatherApiService/src/test/java/com/skyapi/weatherforecast/location/LocationApiControllerTest.java
+++ WeatherApiProject/WeatherApiService/src/test/java/com/skyapi/weatherforecast/location/LocationApiControllerTest.java
@@ -1,14 +1,27 @@
 package com.skyapi.weatherforecast.location;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.skyapi.weatherforecast.common.Location;
@@ -34,5 +47,28 @@ public class LocationApiControllerTest {
 		
 		mockMvc.perform(post(END_POINT_PATH).contentType("application/json").content(bodyContent))
 			.andExpect(status().isBadRequest()).andDo(print());
+	}
+	
+	@Test
+	public void testAddShouldReturn201Created() throws Exception {
+		Location location = new Location();
+		location.setCode("NYC_USA");
+		location.setCityName("New York City");
+		location.setRegionName("New York");
+		location.setCountryCode("US");
+		location.setCountryName("United States of America");
+		location.setEnabled(true);
+		
+		Mockito.when(service.add(location)).thenReturn(location);
+		
+		String bodyContent = mapper.writeValueAsString(location);
+		
+		mockMvc.perform(post(END_POINT_PATH).contentType("application/json").content(bodyContent))
+		.andExpect(status().isCreated())
+		.andExpect(content().contentType("application/json"))
+		.andExpect(jsonPath("$.code", is("NYC_USA")))
+		.andExpect(jsonPath("$.city_name", is("New York City")))
+		.andExpect(header().string("Location", "/v1/locations/NYC_USA"))
+		.andDo(print());		
 	}
 }
