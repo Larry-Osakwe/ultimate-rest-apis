e096d6b040666c616041e9f03496842031c37072
Larry Osakwe <larryosak@gmail.com>
2023-08-04 18:02:57 -0700

    add validations for locartion request body

--- WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
+++ WeatherApiProject/WeatherApiCommon/src/main/java/com/skyapi/weatherforecast/common/Location.java
@@ -2,7 +2,10 @@ package com.skyapi.weatherforecast.common;
 
 import java.util.Objects;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -17,27 +20,28 @@ import jakarta.validation.constraints.NotNull;
 public class Location {
 	@Column(length = 12, nullable = false, unique = true)
 	@Id
-	@NotBlank
+	@NotNull(message = "Location code cannot be null")
+	@Length(min = 3, max = 12, message = "Location code must have 3-12 characters")
 	private String code;
 
-	@Column(length = 128, nullable = false)
 	@JsonProperty("city_name")
-	@NotBlank
+	@NotNull(message = "City name cannot be null")
+	@Length(min = 3, max = 128, message = "City name must have 3-128 characters")
 	private String cityName;
 
-	@Column(length = 128)
 	@JsonProperty("region_name")
-	@NotNull
+	@Length(min = 3, max = 128, message = "Region name must have 3-128 characters")
+	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private String regionName;
 
-	@Column(length = 64, nullable = false)
 	@JsonProperty("country_name")
-	@NotBlank
+	@NotNull(message = "Country name cannot be null")
+	@Length(min = 3, max = 64, message = "Country name must have 3-64 characters")
 	private String countryName;
 
-	@Column(length = 2, nullable = false)
 	@JsonProperty("country_code")
-	@NotBlank
+	@NotNull(message = "Country code cannot be null")
+	@Length(min = 2, max = 2, message = "Country code must have 2 characters")
 	private String countryCode;
 
 	private boolean enabled;
